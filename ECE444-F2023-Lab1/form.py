dd14e2be9abf23e8577253e259c9a634dbedf58f
Zian Zhou <zianzhou@Zians-MacBook-Pro.local>
2023-09-19 20:52:40 -0400

    Add email functionality

--- ECE444-F2023-Lab1/form.py
+++ ECE444-F2023-Lab1/form.py
@@ -3,12 +3,13 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, EmailField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = EmailField("What is your UofT Email address?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
@@ -23,11 +24,20 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
+        old_email = session.get("email")
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
         session["name"] = form.name.data
+        session["email"] = form.email.data
         return redirect(url_for("index"))
-    return render_template("form_index.html", form=form, name=session.get("name"))
+    return render_template(
+        "form_index.html",
+        form=form,
+        name=session.get("name"),
+        email=session.get("email"),
+    )
 
 
 @app.route("/user/<name>")
